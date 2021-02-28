7079b0affabc476b4ad417a48fc5d93201cf1e52
Darkwin <Darkwin.pro@gmail.com>
2021-02-28 12:37:08 +0300

    gun

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _ballPrefab;
+
+    [SerializeField] 
+    private float _power = 10f;
+    
+    private Camera mainCamera;
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+    
+    void Update()
+    {
+        float _enter;
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        new Plane(-Vector3.up, transform.position).Raycast(ray, out _enter);
+        Vector3 _mouseInWorld = ray.GetPoint(_enter);
+
+        Vector3 _speed = (_mouseInWorld - transform.position) * _power;
+        transform.rotation = Quaternion.LookRotation(_speed);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Rigidbody ball = Instantiate(_ballPrefab, transform.position, Quaternion.identity)
+                .GetComponent<Rigidbody>();
+            ball.AddForce(_speed, ForceMode.VelocityChange);
+            
+        }
+    }
+}
