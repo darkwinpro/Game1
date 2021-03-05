61f2f8b627289c3095dd5abaf65055f69278cbf2
Darkwin <Darkwin.pro@gmail.com>
2021-03-05 20:35:22 +0300

    Road Generator

--- Assets/Scripts/RoadGenerator.cs
+++ Assets/Scripts/RoadGenerator.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class RoadGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private GameObject _roadPrefab;
+    [SerializeField]
+    private float _maxSpeedPlayer = 10;
+    [SerializeField]
+    private float _speedPlayer = 0;
+    [SerializeField]
+    private byte _maxRoadCount = 5;
+
+    private List<GameObject> roads = new List<GameObject>();
+
+    private void CreateNextRoad()
+    {
+        Vector3 positionRoad = Vector3.zero;
+        if (roads.Count > 0)
+        {
+            positionRoad = roads[roads.Count - 1].transform.position + new Vector3(0, 0, 15);
+        }
+        GameObject go = Instantiate(_roadPrefab, Vector3.zero, Quaternion.identity);
+        go.transform.SetParent(transform);
+        roads.Add(go);
+        
+    }
+    public void ResetLevel()
+    {
+        _speedPlayer = 0;
+        while (roads.Count > 0)
+        {
+            Destroy(roads[0]);          //удаляем сам обьект
+            roads.RemoveAt(0);      //удаляем из списка
+        }
+
+        for (int i = 0; i < _maxRoadCount; i++)
+        {
+            CreateNextRoad();
+        }
+    }
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (_speedPlayer == 0) 
+            return;
+        foreach (GameObject road in roads)
+        {
+            road.transform.position -= new Vector3(0, 0, _speedPlayer * Time.deltaTime);
+        }
     }
 }
